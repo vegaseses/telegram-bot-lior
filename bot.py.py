0d5022c37474e68a2b3890a64286872f7f150325
vegaseses <craneo.white@gmail.com>
2025-07-18 19:04:21 +0330

    Update bot.py.py

--- bot.py.py
+++ bot.py.py
@@ -1,13 +1,1 @@
-from telegram.ext import Updater, CommandHandler
 
-TOKEN = '7659087531:AAEgrHADdHUk2eu73RxMuA_KS45pkIz8q0Q'
-
-def start(update, context):
-    update.message.reply_text("Welcome to Lior Robot")
-
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-
-updater.start_polling()
-updater.idle()
